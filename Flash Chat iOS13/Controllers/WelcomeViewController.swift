88be3596ff3a95951c9d90e5c7c18214c3f4646c
João Pedro Giarrante <joao.giarrante@valemobi.com.br>
2020-09-23 09:10:59 -0300

    installing pods

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,23 +7,16 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let titleText = titleLabel.text as String? {
-            titleLabel.text = ""
-            for i in 0...titleText.count-1 {
-                Timer.scheduledTimer(withTimeInterval: 0.2 * Double(i), repeats: false) { (timer) in
-                    self.titleLabel.text?.append(titleText[titleText.index(titleText.startIndex, offsetBy: i)])
-                }
-                
-            }
-        }
+        titleLabel.text = "⚡️FlashChat"
         
     }
     
--- Podfile
+++ Podfile
@@ -4,5 +4,9 @@ source 'https://github.com/CocoaPods/Specs.git'
 
 target 'Flash Chat iOS13' do
   use_frameworks!
+  
+  # pods for Flash Chat iOS13
   pod 'CLTypingLabel', '~> 0.4.0'
+  pod "Firebase/Auth"
+  pod "Firebase/Firestore"
 end
